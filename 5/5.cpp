1f1d3c8ce954116f0f3d739ccf7672e079abb122
Gabriela Salguero <37200473+Hanyauku123@users.noreply.github.com>
2020-09-30 07:40:25 -0600

    Update 5.cpp

--- 5/5.cpp
+++ 5/5.cpp
@@ -14,11 +14,22 @@ struct node {
     }
 };
 
-void preOrder(node*);
-void showNodes(node*);
-int levelOfTree(node*);
-void insertData(int, node**);
+struct nodeCola
+{
+    int nivel;
+    int data;
+    nodeCola *next;
 
+};
+
+void preOrder(node*);
+void insertData(int, node**);
+void levelOfTree(node*, int);
+//int levelOfTree(node*);
+void insertInCola( nodeCola **, int, int);
+void printLista(nodeCola*);
+
+nodeCola * listaOrdenada;
 
 int main(void){
     node* tree = nullptr;
@@ -34,12 +45,13 @@ int main(void){
     insertData(65, &tree);
     insertData(100, &tree);
 
-     cout << "******************** PreOrder traversal 1 ********************" << endl;
+    cout << "******************** PreOrder traversal 1 ********************" << endl;
     preOrder(tree);
 
     cout << "******************** Cantidad de niveles ********************" << endl;
-    levelOfTree(tree);
-
+    levelOfTree(tree, 0);
+    //printLista(listaOrdenada);
+    
     return 0;
 }
 
@@ -63,7 +75,7 @@ void insertData(int number, node** root){
     }
 }
 
-int levelOfTree(node* level){
+/*int levelOfTree(node* level){
     if(!level){
         return 0;
     }else if(level->left!= NULL){
@@ -72,4 +84,64 @@ int levelOfTree(node* level){
         return 1 + levelOfTree(level->right);
     }
     return 0;
+}*/
+
+/*void levelOfTree(node *level, int n){
+    if(level){
+        cout << "nivel: " << n << endl;
+        cout << level->data << endl;
+        levelOfTree(level->right, ++n);
+        levelOfTree(level->left, ++n);
+    }
+}*/
+
+
+
+//funci'on que calcula los niveles y toma los valores de los nodos
+void levelOfTree(node *level, int n){
+    if (level){
+        insertInCola(&listaOrdenada, level->data, n);
+        cout << "Nivel: " << n << endl;
+        cout << level->data << endl;
+        levelOfTree(level->left, ++n);
+        levelOfTree(level->right, ++n);
+    }
+}
+
+void insertInCola( nodeCola ** lista, int valor, int nivel){
+    nodeCola * recl = (*lista);
+    nodeCola * ant = recl;
+    cout << "hola" << endl;
+    if(!recl){
+        *lista = new nodeCola();
+        (*lista)->data = valor;
+        (*lista)->nivel = nivel;
+        (*lista)->next = NULL;
+        cout << (*lista)->nivel << endl;
+
+    }
+    //while no ocurre
+    while(true){
+        if(recl->nivel > nivel || recl == NULL ){
+            cout << "hola"<<endl;
+            nodeCola * aux = recl;
+            nodeCola * nuevo = new nodeCola();
+            nuevo->data = valor;
+            nuevo->nivel = nivel;
+            nuevo->next = aux;
+            ant->next = nuevo;
+            break;
+        }
+        ant = recl;
+        recl = recl->next;
+    }
+}
+
+//funcion que imprime la cola con sus niveles
+void printLista(nodeCola * lista){
+    nodeCola * aux = lista;
+    while(aux){
+        cout << "Nivel: " << lista->nivel << "Dato:" << lista->data << endl;
+        aux = aux->next;
+    }
 }
